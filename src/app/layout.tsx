193cd7ea4080dcd6ad68c27681e88f5a6628d2cd
Khunasin Sooksri <86812723+Bearior@users.noreply.github.com>
2026-02-24 15:48:42 +0700

    feat: Update Open Graph metadata for improved SEO and social sharing

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,6 +15,21 @@ export const metadata: Metadata = {
   title: "NPMx Technologies — Innovate. Build. Scale.",
   description:
     "NPMx Technologies delivers modern web development solutions — from static websites to large-scale enterprise systems.",
+  openGraph: {
+    title: "NPMx Technologies — Innovate. Build. Scale.",
+    description: "Custom software, AI solutions, and scalable systems for modern businesses.",
+    url: "https://www.npmxtech.com/",
+    siteName: "NPMx Technologies",
+    images: [
+      {
+        url: "https://scontent.fbkk12-1.fna.fbcdn.net/v/t39.30808-6/641440044_122093559873261574_3468879041835137306_n.jpg?_nc_cat=101&ccb=1-7&_nc_sid=1d70fc&_nc_ohc=KmRPYopSi3YQ7kNvwEfgUvj&_nc_oc=Adk2FAx7wga9Ts_qU7bcRf6PYfp_WjLFC7jliRJRAFGYsD-KHK2OV0ohtGYT_39v3LojoqZ7hghSPpBs0ORDdti6&_nc_zt=23&_nc_ht=scontent.fbkk12-1.fna&_nc_gid=bxUmxQUMnYTFbtgNYLOZ8g&oh=00_AftH5q2MI9edTvDRon9UvdXH3O-n6YXspm5suXjap6xQ_Q&oe=69A33448", // MUST be absolute URL
+        width: 1200,
+        height: 630,
+      },
+    ],
+    locale: "en_US",
+    type: "website",
+  },
 };
 
 export default function RootLayout({
