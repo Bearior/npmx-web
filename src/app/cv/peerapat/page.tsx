19a3407dd85b3058fe33c7d19e0931350995bdcb
Khunasin Sooksri <86812723+Bearior@users.noreply.github.com>
2026-02-24 12:47:21 +0700

    fix : Production build

--- src/app/cv/peerapat/page.tsx
+++ src/app/cv/peerapat/page.tsx
@@ -1,11 +1,7 @@
 "use client";
 
-import { useEffect, useState } from "react";
-import Button from "@mui/material/Button";
-import ArrowBackIcon from "@mui/icons-material/ArrowBack";
-import DownloadIcon from "@mui/icons-material/Download";
+import { useEffect } from "react";
 export default function PeerapatCVPage() {
-  const [isMobile, setIsMobile] = useState(false);
 
   useEffect(() => {
     window.location.replace("https://drive.google.com/file/d/1aJLGEax7msYZ5ZDJVOfNutWaQEx0Fatm/view?usp=sharing");
